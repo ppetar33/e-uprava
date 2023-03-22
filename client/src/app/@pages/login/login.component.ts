9cff18908739b5340ec1927631ee2435fcf787b2
ppetar33 <ppetar33@gmail.com>
2023-03-22 21:23:44 +0100

    * Login implemented

--- client/src/app/@pages/login/login.component.ts
+++ client/src/app/@pages/login/login.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatDialog, MatDialogConfig, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/@api/services/auth.service';
+import { RegisterComponent } from '../register/register.component';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +11,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  public loginForm: FormGroup;
+  public emailExists: boolean = false;
+  public errorMessage: string = '';
+  public isLoading: boolean = false;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    public dialogRef: MatDialogRef<LoginComponent>,
+    public dialog: MatDialog,
+    private router: Router,
+    private authService: AuthService,
+    private formBuilder: FormBuilder,
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) { 
+    this.loginForm = this.formBuilder.group({
+      jmbg: ["", [Validators.required]],
+      password: ["", [Validators.required]],
+    });
   }
 
+  public ngOnInit(): void {
+  }
+
+  public closeDialog(): void {
+    this.dialogRef.close();
+  }
+
+  public submit(): void {
+
+  }
+
+  public openRegistration(): void {
+    this.closeDialog();
+    const dialogRef = new MatDialogConfig();
+    dialogRef.disableClose = true;
+    this.dialog.open(RegisterComponent, dialogRef); 
+  }
 }
