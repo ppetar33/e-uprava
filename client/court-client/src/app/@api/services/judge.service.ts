c55222569f780a5333086362f6c1576865a5ee4e
ppetar33 <ppetar33@gmail.com>
2023-05-03 22:04:37 +0200

    * Implement get all communal problems by judge

---
+++ client/court-client/src/app/@api/services/judge.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class JudgeService {
+  private readonly APIurl = `${environment.APIurl}/court`;
+
+  constructor(
+    private httpClient: HttpClient
+  ) { }
+
+  public getJudgeCommunalProblems(judgeId: string): Observable<any> {
+    return this.httpClient.get(`${this.APIurl}/communal-problem/judge/${judgeId}`);
+  }
+}
